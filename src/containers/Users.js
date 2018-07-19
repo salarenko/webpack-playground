e67c6699bd182b65a1353fa165177c34f908e08e
Salamon, Michal <michal.salamon.external@atos.net>
2018-07-19 11:12:33 +0200

    Basic webpack setup

---
+++ src/containers/Users.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+import PizzaImage from '../components/PizzaImage/PizzaImage';
+
+class Users extends Component {
+    render() {
+        return (
+            <div>
+                <h1>The Users</h1>
+                <p>Users list:</p>
+            </div>
+        );
+    }
+}
+
+export default Users;
